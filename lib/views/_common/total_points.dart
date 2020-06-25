7b2bda3f4d17dd3849409bba27186fb6265a0d20
Justin Seawell <jpseawell@gmail.com>
2020-06-25 06:02:00 -0500

    Fix issue with points showing null on load

--- lib/views/_common/total_points.dart
+++ lib/views/_common/total_points.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:lloud_mobile/util/dal.dart';
 import 'package:lloud_mobile/providers/points.dart';
 
 class TotalPoints extends StatefulWidget {
@@ -13,8 +12,8 @@ class _TotalPointsState extends State<TotalPoints> {
   @override
   Widget build(BuildContext context) {
     final points = Provider.of<Points>(context).points;
-    return Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[Text(points.toString())]);
+    return Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+      (points != null) ? Text(points.toString()) : Text("")
+    ]);
   }
 }
