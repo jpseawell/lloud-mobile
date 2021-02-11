31cdba290388209d963f455d505ca86242734be7
Justin Seawell <jpseawell@gmail.com>
2021-02-11 09:12:22 -0600

    Update host to prod server

--- lib/util/network.dart
+++ lib/util/network.dart
@@ -1,6 +1,6 @@
 class Network {
-  static const host = 'http://dev.lloudapp.com';
-  static const wsHost = 'ws://dev.lloudapp.com';
+  static const host = 'https://www.lloudapp.com';
+  static const wsHost = 'ws://www.lloudapp.com';
 
   static Map<String, String> headers({String token}) {
     Map<String, String> headers = {
